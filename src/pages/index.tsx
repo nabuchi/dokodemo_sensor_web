fba94714b7b72f77f8168aadbb5892397dd6703c
nabuchi <nabnab.main+github@gmail.com>
2020-06-21 21:27:15 +0900

    add netlify settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 function HomePage() {
-  return (<div>Welcome to Next.js!!!</div>)
+  return (<div>Welcome to Next.js!!!!!</div>)
 }
 
 export default HomePage
